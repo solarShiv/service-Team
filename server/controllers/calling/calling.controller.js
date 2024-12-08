e043963a719b8093d237e695bfe56042feb1b098
shiv <shivhack999@gmail.com>
2024-12-08 22:26:36 +0530

    vC

--- server/controllers/calling/calling.controller.js
+++ server/controllers/calling/calling.controller.js
@@ -4,6 +4,12 @@ const verifyComplaint = async(req,res) =>{
   try {
     const empId = req.empId;
     const {complaintId, remark, farmerCertified} = req.body;
+    if(!complaintId || !farmerCertified){
+      return res.status(500).json({
+        success:false,
+        message:'Complaint Id and former certification data required.'
+      })
+    }
     const data ={
       complaintId, 
       remark, 
--- server/routers/calling.router.js
+++ server/routers/calling.router.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const multer = require('multer');
 
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
 const tokenVerify = require('../middlewares/auth/tokenVerify');
 const callingController = require('../controllers/calling/calling.controller.js');
 router.post("/verifyComplaint", tokenVerify, callingController.verifyComplaint);
