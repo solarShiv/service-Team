2774c88dd48504255f373e2b1faf6277873db194
shiv <shivhack999@gmail.com>
2024-12-09 17:42:34 +0530

    x

--- client/src/Pages/ComplaintList/Index.jsx
+++ client/src/Pages/ComplaintList/Index.jsx
@@ -30,7 +30,7 @@ const Index = () => {
             navigate('/dashboard/complaintDetails',{state: complaintId});
         }
         else{
-            navigate('/dashboard/verifyComlaint',{state: complaintId});
+            navigate('/dashboard/verifyComplaint',{state: complaintId});
         }
     }
     return (
